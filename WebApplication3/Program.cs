5f040196a1ef9eb2336311f1079634c40e1ec021
“Khalilnkhgit config --global user.email “khalilnkh@code.edu.az <khalilnkh@code.edu.az>
2023-11-22 21:09:02 +0900

    CreatedNewNetTask

---
+++ WebApplication3/Program.cs
@@ -0,0 +1,18 @@
+namespace WebApplication3
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+            app.UseDefaultFiles();
+
+            app.UseStaticFiles();
+           
+
+
+            app.Run();
+        }
+    }
+}
